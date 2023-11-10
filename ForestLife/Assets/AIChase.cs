84cee52c34105cafab0bf20ed3e768810951e51f
Skulldozer09 <zg1671a@greenwich.ac.uk>
2023-11-10 14:50:31 +0000

    Tilemaps and enemy

--- ForestLife/Assets/AIChase.cs
+++ ForestLife/Assets/AIChase.cs
@@ -9,13 +9,6 @@ public class AIChase : MonoBehaviour
     public GameObject player;
     public float speed;
 
-    public Transform attackPoint;
-    public float attackRange = 1f;
-    public LayerMask playerC;
-    public int attackDamage = 10;
-    public float attackRate = 2f;
-    float nextAttackTime = 0f;
-
     private float distance;
     // Start is called before the first frame update
     void Start()
@@ -39,25 +32,6 @@ public class AIChase : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        if(other.collider.tag == "player") //Weird error happened here, now fixed
-        {
-            Attack();
-            nextAttackTime = Time.time + 1f / attackRate;
-        }
-    }
-
-    void Attack()
-    {
-        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange, playerC);
-
-        foreach (Collider Player in hitPlayer)
-        {
-            Player.GetComponent<Health>().TakeDamage(attackDamage);
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -66,13 +40,5 @@ public class AIChase : MonoBehaviour
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
 
-        if (Time.time >= nextAttackTime)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Attack();
-                nextAttackTime = Time.time + 1f / attackRate;
-            }
-        }
     }
 }
